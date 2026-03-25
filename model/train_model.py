cd0173c9365c18c209cc3104b6efc4978de6e941
Ayush Chouhan <ayush.25bai10415@vitbhopal.ac.in>
2026-03-25 10:44:12 +0530

    Update train_model.py

--- model/train_model.py
+++ model/train_model.py
@@ -1,1 +1,27 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import pickle
 
+# Load data
+data = pd.read_csv("data/housing.csv")
+
+# Convert categorical to numeric
+data = pd.get_dummies(data, drop_first=True)
+
+# Features & Target
+X = data.drop("price", axis=1)
+y = data["price"]
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Save
+pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(X.columns, open("columns.pkl", "wb"))
+
+print("Model trained successfully!")
